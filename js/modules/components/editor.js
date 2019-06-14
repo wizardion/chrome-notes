d9c3a680ecae43568d3cc9475669c99e0e4abd6d
Alex Zarnitsa <alex@zarnitsa.net>
2019-06-13 20:35:38 -0400

    Working with tags

--- js/modules/components/editor.js
+++ js/modules/components/editor.js
@@ -14,36 +14,43 @@ class Editor {
     // });
 
     // // Remove all attributes except allowed.
-    // var replacements = ['$1'];
-    // var attrPatterns = [];
+    var replacements = ['$1'];
+    var attrPatterns = [];
 
-    // for(var i = 0; i < allowedAttributes.length; i++){
-    //   replacements.push('$' + (i + 2));
+    for(var i = 0; i < allowedAttributes.length; i++){
+      replacements.push('$' + (i + 2));
 
-    //   attrPatterns.push(
-    //       '(?:(?:(?:(?!' + allowedAttributes.join('=|') + '=)[^>]))*((?:' + 
-    //       allowedAttributes.join('|') + ')=[\'"][^\'"]*[\'"]\\s*)?)'
-    //   );
-    // }
+      attrPatterns.push(
+          '(?:(?:(?:(?!' + allowedAttributes.join('=|') + '=)[^>]))*((?:' + 
+          allowedAttributes.join('|') + ')=[\'"][^\'"]*[\'"])?)\\s*'
+      );
+    }
 
-    // this.rules.push({
-    //   pattern: new RegExp('<(\\w+)\\s*' + attrPatterns.join('') + '[^>]*>', 'gi'),
-    //   replacement: '<' + replacements.join(' ') + '>'
-    // });
+    this.rules.push({
+      pattern: new RegExp('<(\\w+)\\s*' + attrPatterns.join('') + '[^>]*>', 'gi'),
+      replacement: '<' + replacements.join(' ') + '>'
+    });
 
     // this is a test and <b>Bold</b> and <b style="color: red;">Red-bold</b> and <strike>Strike</strike> < b>Bold2</b> < b>Bold3</ b> < b>Bold3< / b> 
+    // (<\s?(a|b|i|u|strong|br|strike)\s?>)
+    // (\s?[^>]{0,}class="[^<>"']*"[^>]{0,})
     // (<\s?(a|b(\s?[^>]{0,}class="[^<>"']+"[^>]{0,})?|i|u|strong|br|strike)>)
+
+    // (<\s?(a|b)\s?[^>]*(class|href="[^<>"']*")[^>]{0,}\s?>) - with attributes
+
+    // (<\s?(a|b)\s?[^>]*((class|href)="[^<>"']*")[^>]{0,}\s?>)
+    //<(\w+)[^<>]*((?:href)="[^'"]*")[^<>]*>
     
     // Remove all tags except allowed.
     // https://www.regextester.com/93930
-    this.rules.push({
-      // pattern: new RegExp('(<\/?(?:' + allowedTags.join('|') + ')[^>]*>)|<[^>]+>', 'gi'),
-      // pattern: new RegExp('(<\s?(\/?)\s?(a|b|i|u|strong|br|strike)(>|\s[^>]+>))', 'igm'),
-      pattern: /(<\s?(\/?)\s?(a|b|i|u|strong|br|strike)(>|(\s[^>]+)>))/igm,
-      // pattern: new RegExp('(<?(?:a|b|i|u|strong|br|strike)>)([^<>]+<\/)|(<?(?:a|b|i|u|strong|br|strike)\\s[^>]+>)([^<>]+<\/)|<[^>]+>', 'igm'),
-      // pattern: new RegExp('((<(?:a|b|i|u|strong|br|strike)>)|(<(?:a|b|i|u|strong|br|strike)\s[^>]*>))([^<>]+)', 'igm'),
-      replacement: '<$2$3$5>'
-    });
+    // this.rules.push({
+    //   // pattern: new RegExp('(<\/?(?:' + allowedTags.join('|') + ')[^>]*>)|<[^>]+>', 'gi'),
+    //   // pattern: new RegExp('(<\s?(\/?)\s?(a|b|i|u|strong|br|strike)(>|\s[^>]+>))', 'igm'),
+    //   pattern: /(<\s?(\/?)\s?(a|b|i|u|strong|br|strike)(>|(\s[^>]+)>))/igm,
+    //   // pattern: new RegExp('(<?(?:a|b|i|u|strong|br|strike)>)([^<>]+<\/)|(<?(?:a|b|i|u|strong|br|strike)\\s[^>]+>)([^<>]+<\/)|<[^>]+>', 'igm'),
+    //   // pattern: new RegExp('((<(?:a|b|i|u|strong|br|strike)>)|(<(?:a|b|i|u|strong|br|strike)\s[^>]*>))([^<>]+)', 'igm'),
+    //   replacement: '<$2$3$5>'
+    // });
 
     // // Add tab space
     // this.rules.push({
@@ -100,6 +107,8 @@ class Editor {
       for (let index = 0; index < this.rules.length; index++) {
         const rule = this.rules[index];
         data = data.replace(rule.pattern, rule.replacement);
+
+        console.log(`${rule.pattern}\n ${rule.replacement}`);
       }
 
       console.log(`${data}`);
