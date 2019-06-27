32e125e1359a6bab560a97b443f301e6250d8296
Alex Zarnitsa <alex.zarnitsa@kmilearning.com>
2019-06-27 08:16:43 -0400

    Working with editor

--- js/modules/components/editor-base.js
+++ js/modules/components/editor-base.js
@@ -142,7 +142,7 @@ class BaseEditor {
 
     e.preventDefault();
 
-    if (localStorage.pasteHtml || true) {
+    if (localStorage.allowPasteHtml || true) {
       var html = clipboard.getData('text/html');
 
       if (html) {
