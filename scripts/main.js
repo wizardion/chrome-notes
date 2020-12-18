e43fa07354115b9333e6609e0e47521225748904
Alex Zarnitsa <alex@zarnitsa.net>
2020-12-18 18:11:50 -0500

    Revert to production build version.

--- scripts/main.js
+++ scripts/main.js
@@ -1,81 +1,19 @@
-window.addEventListener('load', function(){
-// document.addEventListener('DOMContentLoaded', function(){
-  // var editor = new Editor('.fast-note[0]');
-  // var details = new HtmlElement('.details-view[0]');
+window.onload = function(){
+    //var bgr = chrome.extension.getBackgroundPage();
 
-  // setTimeout(function(){
-  //   var notes = new SimpleNotes({
-  //     back: new HtmlElement('.back[0]'),
-  //     add: new HtmlElement('.add-note[0]'),
-  //     delete: new HtmlElement('.delete[0]'),
-  //     // template: new HtmlElement('.list-item[0]'),
-  //     listView: new HtmlElement('.list-view[0]'),
-  //     listItems: new HtmlElement('.list-items[0]'),
-  //     detailsView: new HtmlElement('.details-view[0]'),
-  //     title: new HtmlElement('.title[0]'),
-  //     description: new HtmlElement('.fast-note[0]')
-  //   });
-  // }, 45);
+    $('#notes').Notes({
+        back_bt: 'back',
+        add_bt: 'add-note',
+        delete_bt: 'delete',
 
-  // var notes = new SimpleNotes({
-  //   back: new HtmlElement('.back[0]'),
-  //   add: new HtmlElement('.add-note[0]'),
-  //   delete: new HtmlElement('.delete[0]'),
-  //   // template: new HtmlElement('.list-item[0]'),
-  //   listView: new HtmlElement('.list-view[0]'),
-  //   listItems: new HtmlElement('.list-items[0]'),
-  //   detailsView: new HtmlElement('.details-view[0]'),
-  //   title: new HtmlElement('.title[0]'),
-  //   description: new HtmlElement('.fast-note[0]')
-  // });
+        title: 'title',
+        edit_title: 'edit-title',
 
-  var notes = new SimpleNotes({
-    back: document.getElementById('to-list'),
-    add: document.getElementById('add-note'),
-    delete: document.getElementById('delete-note'),
-    listView: document.getElementById('list-view'),
-    listItems: document.getElementById('list-items'),
-    detailsView: document.getElementById('details-view'),
-    title: document.getElementById('title-note'),
-    description: document.getElementById('description-note'),
+        list_view: 'list-view',
+        edit_view: 'edit-view',
 
-    search: document.getElementById('search-button'),
-    listControls: document.getElementById('list-controls')
+        description: 'fast-note',
+        list_item: 'list-item'
+    });
+}
 
-    // templates: {
-    //   new: document.getElementById('new-note'),
-    // }
-  });
-
-
-  // var something = function (a, b, c) {
-  //   console.log(a, b, c);
-  // };
-
-  // // a binding of something with 3 defined args
-  // var b = something.bind(this, 1, 2, 3);
-
-  // // call b
-  // b(213);
-
-
-
-
-});
-
-
-
-//     $('#notes', true).Notes({
-//         list_view: $(".list-view")[0],
-//         details_view: $(".details-view")[0],
-//         item_template: $(".list-item")[0],
-//         list_items: $(".list-items")[0],
-//         title_span: $(".title")[0],
-//         description_input: $(".fast-note")[0],
-//         back_button: $(".back")[0],
-//         add_button: $(".add-note")[0],
-//         delete_button: $(".delete")[0],
-//         search_button: $(".search-button")[0],
-//         loading: $(".loading")[0]
-//     });
-// }
