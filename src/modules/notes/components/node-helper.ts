9ec0dc7be0e27310d2a7b9a153c6ea332af8fa6c
Alex Zarnitsa <alex.zarnitsa@kmilearning.com>
2021-04-15 14:18:34 -0400

    fixed previewed selection

--- src/modules/notes/components/node-helper.ts
+++ src/modules/notes/components/node-helper.ts
@@ -19,9 +19,12 @@ export class NodeHelper {
       
       let leftNode: Node = this.getNode(left.split(','), element);
       let rightNode: Node = this.getNode(right.split(','), element);
-      let selection: Selection = document.getSelection();
 
-      selection.setBaseAndExtent(leftNode, Number(start), rightNode, Number(end));
+      if (leftNode && rightNode) {
+        let selection: Selection = document.getSelection();
+
+        selection.setBaseAndExtent(leftNode, Number(start), rightNode, Number(end));
+      }
     }
   }
 
@@ -50,11 +53,11 @@ export class NodeHelper {
     return possition;
   }
 
-  private static getNode(possition: string[], $root: Node): Node {
+  private static getNode(possition: string[], root: Node): Node {
     var index = possition.length - 1;
-    var node = $root.childNodes[Number(possition[index])];
+    var node = root.childNodes[Number(possition[index])];
 
-    for (var i = index - 1; i >= 0; i--) {
+    for (var i = index - 1; i >= 0 && node; i--) {
       node = node.childNodes[Number(possition[i])];
     }
 
