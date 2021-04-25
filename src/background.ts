5630ed99c87151607c2b63380216a7eaaee0d34f
Alex Zarnitsa <alex@zarnitsa.net>
2021-04-25 17:40:53 -0400

    working with start up

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,16 @@
-import db from './modules/db/db'
+// import db from './modules/db/db'
 
-document.addEventListener('DOMContentLoaded', function () {
-  db.init();
-  console.log('DOMContentLoaded');
+// document.addEventListener('DOMContentLoaded', function () {
+//   db.init();
+//   console.log('DOMContentLoaded');
+// });
+
+chrome.alarms.create('alarmName', {periodInMinutes: 2});
+
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  console.log("Got an alarm!", {
+    'name': alarm.name,
+    'periodInMinutes': alarm.periodInMinutes,
+    'scheduledTime': new Date(alarm.scheduledTime).toLocaleTimeString()
+  });
 });
