bcfbbc16db4827ec4d71e860261ce3263f21303c
Alex Zarnitsa <alex@zarnitsa.net>
2022-01-02 15:43:19 -0500

    added storage checker

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,10 @@ window.onload = function(){
     //var bgr = chrome.extension.getBackgroundPage();
 
     if (chrome && chrome.storage) {
+      chrome.storage.local.get(['migrate', 'oldNotes'], function(resutl) {
+        console.log('storage.resutl', resutl);
+      });
+
       chrome.storage.local.set({migrate: true});
     }
 
