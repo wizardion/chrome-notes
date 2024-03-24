0306a62fc47adf51bf8d2e4b43c6f1af326b670e
Alex Zarnitsa <alex@zarnitsa.net>
2024-03-24 17:46:06 -0400

    - fixed lint errors

--- src/components/visual-editor/extensions/markdown.parser.ts
+++ src/components/visual-editor/extensions/markdown.parser.ts
@@ -1,13 +1,8 @@
 import { MarkdownParser } from 'prosemirror-markdown';
 import * as MarkdownIt from 'markdown-it';
-import * as Token from 'markdown-it/lib/token';
 import { schema } from './schema';
 
 
 export const md = new MarkdownIt('commonmark', { html: false });
 
-// console.log('md', md.);
-
 export const markdownParser = new MarkdownParser(schema, md, {});
-
-// console.log('', markdownParser.tokens);
