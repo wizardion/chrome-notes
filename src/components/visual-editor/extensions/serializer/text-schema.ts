53edc598dc7b5bc5d9e94c10c634fe2b7b1b56c1
Alex Zarnitsa <alex@zarnitsa.net>
2024-06-25 21:07:19 -0400

    - refactored MarkdownSerializer in Visual Editor

--- src/components/visual-editor/extensions/serializer/text-schema.ts
+++ src/components/visual-editor/extensions/serializer/text-schema.ts
@@ -4,30 +4,30 @@ import { ISerializingSchema } from './models/serializer.models';
 export const textSerializingSchema: ISerializingSchema = {
   nodes: {
     heading: {
-      toString(content: string) {
-        return `${content}\n`;
+      toString(content: string[]) {
+        return `${content.join('')}\n`;
       }
     },
     bulletList: {
       attrs: { listType: '' },
-      toString(content: string) {
-        return content;
+      toString(content: string[]) {
+        return content.join('\n');
       }
     },
     orderedList: {
       attrs: { listType: '' },
-      toString(content: string) {
-        return content;
+      toString(content: string[]) {
+        return content.join('\n');
       }
     },
     listItem: {
-      toString(content: string) {
-        return content.replace(/\n(?!$)/g, `\n`);
+      toString(content: string[]) {
+        return content.join('\n');
       }
     },
     paragraph: {
-      toString(content: string) {
-        return content + '\n';
+      toString(content: string[]) {
+        return content.join('');
       }
     },
     break: {
@@ -36,13 +36,18 @@ export const textSerializingSchema: ISerializingSchema = {
       }
     },
     codeBlock: {
-      toString(content: string) {
-        return content + '\n';
+      toString(content: string[]) {
+        return content.join('');
       }
     },
     blockquote: {
+      toString(content: string[]) {
+        return content.join('') + '\n';
+      }
+    },
+    text: {
       toString(content: string) {
-        return content + '\n';
+        return content;
       }
     }
   },
@@ -54,22 +59,22 @@ export const textSerializingSchema: ISerializingSchema = {
     },
     code: {
       toString(content: string) {
-        return content.replace(/^(\s*)(\S.+\S)(\s*)$/g, '$1$2$3');
+        return content.replace(/^(\s*)(.+)(\s*)$/g, '$1$2$3');
       }
     },
     strong: {
       toString(content: string) {
-        return content.replace(/^(\s*)(\S.+\S)(\s*)$/g, '$1$2$3');
+        return content.replace(/^(\s*)(.+)(\s*)$/g, '$1$2$3');
       }
     },
     italic: {
       toString(content: string) {
-        return content.replace(/^(\s*)(\S.+\S)(\s*)$/g, '$1$2$3');
+        return content.replace(/^(\s*)(.+)(\s*)$/g, '$1$2$3');
       }
     },
     strike: {
       toString(content: string) {
-        return content.replace(/^(\s*)(\S.+\S)(\s*)$/g, '$1$2$3');
+        return content.replace(/^(\s*)(.+)(\s*)$/g, '$1$2$3');
       }
     }
   }
